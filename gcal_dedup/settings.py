f065cc82b5428a9941eae5cd71375c4f85b4e840
Will Adams <willadams+dev@gmail.com>
2015-01-18 15:28:48 -0500

    style templates with CSS

--- gcal_dedup/settings.py
+++ gcal_dedup/settings.py
@@ -75,6 +75,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
 from settings_secret import *
